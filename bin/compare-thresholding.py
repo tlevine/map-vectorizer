439768cc50dd1831f3511b8f65100fa64e2301a0
Thomas Levine <_@thomaslevine.com>
2016-03-09 21:06:22 +0000

    cli

--- bin/compare-thresholding.py
+++ bin/compare-thresholding.py
@@ -61,16 +61,25 @@ def compare(left, right):
     diff = ImageChops.difference(Image.open(left), Image.open(right))
     return sum(diff.histogram())
 
-def gimp_many():
+def compare_thresholding(output_dir, input_file='test.tif'):
+    '''
+    Run like so.
+    
+        ./bin/compare-thresholding.py ~/gimp-thresholding
+
+    Then check the contents of ~/gimp-thresholding to see what the
+    thresholds look like.
+    '''
     brightness = range(-127, 128, 16)
     contrast = range(0, 100, 10)
     thresholdblack = range(0, 255, 16)
     thresholdwhite = range(0, 255, 16)
 
     for args in product(brightness, contrast, thresholdblack, thresholdwhite):
-        fn = '/tmp/gimp_%d_%d_%d_%d.tif' % args
+        fn = os.path.join(output_dir, 'gimp_%d_%d_%d_%d.tif' % args)
         if not os.path.isfile(fn):
-            gimp_one('test.tif', fn, *args)
+            gimp_one(input_file, fn, *args)
 
 if __name__ == '__main__':
-    gimp_many() # main()
+    import horetu
+    horetu.horetu(compare_thresholding)
